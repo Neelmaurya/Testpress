06e2eb2eb8f0713005a96f7ca4f0df40f7b42c52
Neelmaurya <Sunilmaurya278@gmail.com>
2021-07-05 22:30:35 +0530

    Y

--- quiz_proj/quiz_proj/settings.py
+++ quiz_proj/quiz_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '_op!x%^1gyhwbigcfat)7(*dvll*-2bcze^jf63ru==+ww2wnk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','simplequizapp.herokuapp.com']
 
 
 # Application definition
