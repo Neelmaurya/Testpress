bd5cd996fb126763d12f99aa1fc29ef506853a3c
Neelmaurya <Sunilmaurya278@gmail.com>
2021-07-06 08:21:53 +0530

    Y

--- quiz_proj/settings.py
+++ quiz_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_op!x%^1gyhwbigcfat)7(*dvll*-2bcze^jf63ru==+ww2wnk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','simplequizapp.herokuapp.com']
 
