1919a0e6c8b3a9bdefda45d1f6975196ea7893db
Neelmaurya <Sunilmaurya278@gmail.com>
2021-07-06 20:42:38 +0530

    Y

--- results/models.py
+++ results/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Result(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     score = models.FloatField()
 
     def __str__(self):
